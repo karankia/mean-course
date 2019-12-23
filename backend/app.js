c08e9596afd61881e9ebd1c284955baeec743d90
Karan Sharma <SHKA6013@MUHCAD.MUHCFRD.CA>
2019-12-23 16:25:45 -0500

    Adding express

---
+++ backend/app.js
@@ -0,0 +1,47 @@
+  const express = require("express");
+  const bodyParser = require("body-parser");
+
+  const app = express();
+
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+    next();
+  });
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+
+  app.post("/api/posts", (req, res, next) => {
+    const post = req.body;
+    console.log(post);
+    res.status(201).json({
+      message: "Post added successfully"
+    });
+  });
+
+  app.get('/api/posts', (req, res, next) => {
+    const posts = [
+      {
+        id: '1234',
+        title: 'First Server Side Post',
+        content: 'Coming from server'
+      },
+      {
+        id: "1235",
+        title: "Second server side post",
+        content: 'Coming from server'
+      }
+    ];
+    res.status(200).json({
+      message: 'posts fetched successfully',
+      posts: posts
+    });
+  });
+
+  module.exports = app;
+
+
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -14,10 +14,10 @@ export class PostListComponent implements OnInit, OnDestroy {
   constructor( public postService: PostService) {
   }
   ngOnInit() {
-   this.posts = this.postService.getPosts();
-   this.postsSub = this.postService.getPostUpdateListener().subscribe((posts: Post[]) => {
-     this.posts = posts;
-   });
+    this.postService.getPosts();
+    this.postsSub = this.postService.getPostUpdateListener().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
   }
 
   ngOnDestroy() {
