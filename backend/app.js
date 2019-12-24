400d01a221578c14cac50c41054e4f0cec6183dd
Karan Sharma <SHKA6013@MUHCAD.MUHCFRD.CA>
2019-12-24 13:00:04 -0500

    Adding mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
   const express = require("express");
   const bodyParser = require("body-parser");
+  const mongoose = require('mongoose');
+
+  const Post = require('./models/post');
 
   const app = express();
+
+  mongoose.connect("mongodb+srv://karan:karan123@cluster0-j1bur.mongodb.net/test?retryWrites=true&w=majority")
+    .then(() => {
+      console.log("Connected to DB")
+    })
+    .catch((e) => {
+      console.log("Connection Failed" + e);
+    });
 
   app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
@@ -16,8 +27,11 @@
   app.use(bodyParser.urlencoded({ extended: false }));
 
   app.post("/api/posts", (req, res, next) => {
-    const post = req.body;
-    console.log(post);
+    const post = new Post({
+      title: req.body.title,
+      content: req.body.content
+    });
+    post.save();
     res.status(201).json({
       message: "Post added successfully"
     });
