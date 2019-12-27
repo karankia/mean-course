16a68fb651ad6d6236c1902d9278c03ab430a5b8
Karan Sharma <karankia1@gmail.com>
2019-12-27 11:50:05 -0500

    Adding delete routr

--- backend/app.js
+++ backend/app.js
@@ -38,22 +38,19 @@
   });
 
   app.get('/api/posts', (req, res, next) => {
-    const posts = [
-      {
-        id: '1234',
-        title: 'First Server Side Post',
-        content: 'Coming from server'
-      },
-      {
-        id: "1235",
-        title: "Second server side post",
-        content: 'Coming from server'
-      }
-    ];
-    res.status(200).json({
-      message: 'posts fetched successfully',
-      posts: posts
+    Post.find().then((documents) =>{
+      res.status(200).json({
+        message: 'posts fetched successfully',
+        posts: documents
+      });
     });
+  });
+
+  app.delete('/api/posts/:id', (req, res, next) => {
+    Post.deleteOne({ _id: req.params.id }).then( (result) => {
+      res.status(200).json({ message: "Post Deleted" });
+    });
+
   });
 
   module.exports = app;
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,9 +1,10 @@
-import { Post } from './post.model';
-import { HttpClient } from '@angular/common/http';
+import {Post} from './post.model';
+import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-import { Subject } from 'rxjs';
+import {Subject} from 'rxjs';
+import {map} from 'rxjs/operators';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class PostService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
@@ -12,9 +13,18 @@ export class PostService {
   }
 
   getPosts() {
-   this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-     .subscribe((postData) => {
-      this.posts = postData.posts;
+   this.http.get<{message: string, posts: any}>('http://localhost:3000/api/posts')
+     .pipe(map((postData) => {
+       return postData.posts.map(post => {
+         return {
+           title: post.title,
+           content: post.content,
+           id: post._id
+         };
+       });
+     }))
+     .subscribe((transformedPosts) => {
+      this.posts = transformedPosts;
       this.postsUpdated.next([...this.posts]);
      });
   }
@@ -30,6 +40,15 @@ export class PostService {
         console.log(responseData.message);
         this.posts.push(post);
         this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  deletePost(postId: string) {
+    this.http.delete('http://localhost:3000/api/posts/' + postId)
+      .subscribe(() => {
+        this.posts = this.posts.filter(post => post.id !== postId);
+        this.postsUpdated.next([...this.posts]);
+
       });
   }
 }
